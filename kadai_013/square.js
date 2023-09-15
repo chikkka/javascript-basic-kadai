fd9ddf0d521d5de5f6ac2b0b4654ea8caa8b4ffc
chikkka <141250393+chikkka@users.noreply.github.com>
2023-09-15 13:55:20 +0900

    square.jsをアップロード

---
+++ kadai_013/square.js
@@ -0,0 +1,13 @@
+//教材補手本に作成
+const result = (num) =>{
+ console.log(num**2);
+ }
+result(10);
+
+//講義を手本に作成
+function anser(num){
+  const anser =num**2;
+  return anser;
+}
+console.log(anser(10));
+
